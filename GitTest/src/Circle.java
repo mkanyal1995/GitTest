f372a4849ab1458374eb68d3a0e59fce28741b90
mkanyal1995 <66062406+mkanyal1995@users.noreply.github.com>
2021-04-27 07:52:44 +0530

    Update Circle.java

--- GitTest/src/Circle.java
+++ GitTest/src/Circle.java
@@ -3,10 +3,5 @@ public class Circle implements Shape {
    @Override
    public void draw() {
       System.out.println("Inside Circle::draw() method.");
-<<<<<<< HEAD
-      System.out.println("Inside Circle::draw() method. for merging conflicts changing in original workspace");
-=======
-      System.out.println("Inside Circle::draw() method. for merging conflicts changing in cloned workspace");
->>>>>>> 00371315d905be8eea41d4c7ea23580699e306fc
    }
 }
